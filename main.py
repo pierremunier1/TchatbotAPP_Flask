e912206809dfa6be66d0c7d81914947fdc27b33c
pierro145 <pierremunier@hotmail.fr>
2020-05-14 23:16:18 +0200

    UPDATE

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from tchatbotapp import tchatbotapp
 
-elif os.environ.get('ENV') == 'PRODUCTION':
-    DEBUG = False 
-   else:
-        DEBUG = True
-
 if __name__ == "__main__":
-   tchatbotapp.run(debug=DEBUG)
+   tchatbotapp.run(debug=False)
    
     
     

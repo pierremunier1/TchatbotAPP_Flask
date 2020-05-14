6d9945374cb7fdd272adce5ad54b38bc715fa5ea
pierro145 <pierremunier@hotmail.fr>
2020-05-14 10:37:18 +0200

    update html

--- tchatbotapp/view.py
+++ tchatbotapp/view.py
@@ -8,7 +8,7 @@ class Front:
 
     @tchatbotapp.route('/')
     def index():
-        return render_template('brouillon.html')
+        return render_template('index.html')
 
     @tchatbotapp.route("/ajax", methods=["POST"])
     def ajax():
